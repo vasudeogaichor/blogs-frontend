97f6d94f2ac67d0e7f295c9cf5438b91ecc7f6bd
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-11-18 00:16:44 +0530

    get posts from API

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,34 @@
+import { useState, useEffect } from 'react';
 import PostListItem from "./PostListItem"
 
+const API_URL = 'http://localhost:5000/posts';
+
+const getPosts = async () => {
+    const res = await fetch(API_URL)
+    const posts = await res.json()
+    return posts.message
+}
+
 const PostList = () => {
+
+    const [allPosts, setAllPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const posts = await getPosts();
+                setAllPosts(posts);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
-        <PostListItem />
-        <PostListItem />
-        <PostListItem />
-        <PostListItem />
-        <PostListItem />
+            {allPosts.map((post) => <PostListItem key={post?.id} title={post?.title} content={post.content} createdAt={post.created_at} />)}
         </>
     )
 }
--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,29 +1,12 @@
-const PostListItem = () => {
+const PostListItem = ({title, content, createdAt}) => {
+    const trimmedContent = content.trim(100)
     return (
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-                <h3 class="mb-0">Featured post</h3>
-                <div class="mb-1 text-muted">Nov 12</div>
-                <p class="card-text mb-auto">Duis aliqua eiusmod est officia nostrud sit sint proident dolore proident qui non. Laborum amet nisi velit id nostrud dolor culpa aliqua consectetur duis nisi nostrud consectetur. Incididunt consequat amet aliqua quis fugiat est irure. Dolor amet officia sint velit nostrud labore mollit nulla veniam irure aliquip eiusmod.
-
-                    Ea cillum proident commodo consectetur aliqua sint nisi. Consequat do mollit ut velit cillum et cillum qui do qui irure. Ea et consectetur fugiat et tempor laborum aute mollit aliqua culpa sit. Officia ut qui aute culpa incididunt labore. Esse pariatur adipisicing in cillum sint deserunt adipisicing elit ut ullamco laboris elit do occaecat. Culpa amet ad do nisi exercitation laborum sint.
-
-                    Aute adipisicing et proident sunt in consequat nulla laboris. Laborum elit qui eu ad incididunt in aute culpa ad deserunt ex fugiat. Esse consequat anim velit in. Excepteur adipisicing aliquip eiusmod quis occaecat mollit laboris non occaecat sunt commodo eiusmod. Fugiat nulla sint laboris excepteur anim consectetur. Nostrud dolor Lorem fugiat eu consequat labore officia mollit dolore eu sit exercitation culpa.
-
-                    Irure cillum ullamco do labore consequat dolor enim ea. Ut amet tempor aute ullamco ex mollit proident laboris velit quis. Aliquip commodo reprehenderit amet sit eiusmod aute dolore ea sit commodo aliquip consectetur dolor.
-
-                    Fugiat minim esse duis voluptate in dolor incididunt non ipsum in Lorem. Ipsum veniam ea adipisicing do aute. Ex do cupidatat incididunt qui minim in in consectetur excepteur id. Exercitation proident dolore nisi dolore officia consectetur amet sint id. Nostrud excepteur occaecat Lorem officia id ea ea irure labore. Consequat nostrud minim consequat adipisicing nulla est pariatur ea sint quis pariatur mollit. Ad officia adipisicing consectetur nostrud ea eiusmod et ea nulla.
-
-                    Consequat esse fugiat sit culpa Lorem sunt nulla nostrud minim sunt velit cupidatat. Anim velit sunt dolore mollit ad cillum veniam proident. Deserunt est in amet esse ea nostrud adipisicing. Pariatur dolor in non labore pariatur ea ullamco nostrud pariatur. Aliqua qui ex in sit minim aliquip sit dolore nulla laboris nostrud reprehenderit. Et cupidatat incididunt ullamco nostrud.
-
-                    Ad aute dolore sit do do sint sint aliqua qui laboris. Veniam quis ad laborum aliquip consequat id enim. Et incididunt labore dolor cupidatat aliquip eu aliqua qui ea.
-
-                    Anim duis sint nisi non quis laboris labore ea qui. Dolor fugiat ullamco pariatur tempor proident. Consectetur nulla amet laboris anim pariatur in aliquip eiusmod velit ullamco irure officia ipsum consequat.
-
-                    Minim proident ipsum consectetur anim commodo adipisicing ea Lorem id sint aute. Nostrud reprehenderit commodo Lorem ullamco. Nisi occaecat reprehenderit exercitation cupidatat Lorem id nulla in. Cupidatat fugiat sit minim sint sint et consectetur ea in Lorem. Ullamco laborum in fugiat et tempor culpa deserunt eu cillum Lorem eu.
-
-                    Nisi ea velit magna anim ex ipsum ullamco aute commodo consequat veniam qui. In excepteur dolor qui laboris do id cupidatat labore et cupidatat. Quis nostrud dolor ex cupidatat deserunt esse nulla nostrud laboris velit sunt officia ea. Irure ipsum magna magna cillum aliqua enim dolore. Labore in ea incididunt veniam aliquip cupidatat qui eiusmod laboris ad. Lorem exercitation fugiat deserunt do ea et. Labore voluptate veniam velit veniam velit deserunt et laboris minim laborum nulla laboris.</p>
-                <a href="#" class="stretched-link">Continue reading</a>
+        <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <div className="col p-4 d-flex flex-column position-static">
+                <h3 className="mb-0">{title}</h3>
+                <div className="mb-1 text-muted">{createdAt}</div>
+                <p className="card-text mb-auto">{trimmedContent}</p>
+                <a href="#" className="stretched-link">Continue reading</a>
             </div>
         </div>
     )
