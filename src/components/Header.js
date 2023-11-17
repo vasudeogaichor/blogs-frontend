6605b38eeca1f83c3cacda05366c2605d702f294
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-11-17 14:45:07 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+const Header = () => {
+    return (
+        <header className="d-flex flex-wrap justify-content-between py-3 mb-4 border-bottom">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src="logo.svg" alt="Posts Logo" width="30" height="24" />
+                        Posts
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Create</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
