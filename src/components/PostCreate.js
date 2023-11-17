de42cfae7a4ede25da1b2b7981de0f2df0cce5f3
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-11-17 23:47:12 +0530

    small fixes

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,21 +1,21 @@
 const PostCreate = () => {
     return (
         <>
-            <div class="container mt-5">
-                <h2 class="mb-4">Create a new post</h2>
+            <div className="container mt-5">
+                <h2 className="mb-4">Create a new post</h2>
 
                 <form>
-                    <div class="mb-3">
-                        <label for="title" class="form-label h6">Title:</label>
-                        <input type="text" class="form-control" id="title" placeholder="Enter title" />
+                    <div className="mb-3">
+                        <label for="title" className="form-label h6">Title:</label>
+                        <input type="text" className="form-control" id="title" placeholder="Enter title" />
                     </div>
 
-                    <div class="mb-3">
-                        <label for="content" class="form-label h6">Content:</label>
-                        <textarea class="form-control" id="content" rows="8" placeholder="Enter content"></textarea>
+                    <div className="mb-3">
+                        <label for="content" className="form-label h6">Content:</label>
+                        <textarea className="form-control" id="content" rows="8" placeholder="Enter content"></textarea>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         </>
