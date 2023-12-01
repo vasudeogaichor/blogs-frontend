2d5b1a8bbcdff99722699316ae8d9b902349336b
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-01 17:39:21 +0530

    added delete blog operation

--- src/apis/blogs.js
+++ src/apis/blogs.js
@@ -1,29 +1,41 @@
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Create a new blog
 export const createBlog = async (blog) => {
-    const res = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(blog),
-    });
-    const data = await res.json();
-    return data.message;
-  }
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(blog),
+  });
+  const data = await res.json();
+  return data.message;
+};
 
 // Get single blog
 export const getBlog = async (id) => {
-    const res = await fetch(`${API_URL}/${id}`);
-    const blog = await res.json()
-    return blog.message
-}
+  const res = await fetch(`${API_URL}/${id}`);
+  const blog = await res.json();
+  return blog.message;
+};
 
 // List a number of blogs
 export const listBlogs = async (queryParams) => {
-    const queryString = new URLSearchParams(queryParams).toString();
-    const res = await fetch(`${API_URL}?${queryString}`)
-    const blogs = await res.json()
-    return blogs
-}
+  const queryString = new URLSearchParams(queryParams).toString();
+  const res = await fetch(`${API_URL}?${queryString}`);
+  const blogs = await res.json();
+  return blogs;
+};
+
+// Delete a blog
+export const deleteBlog = async (id) => {
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+  const status = res.status;
+  return status
+};
