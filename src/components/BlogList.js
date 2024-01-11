a3c64e6b94fa8213202380c43154d574345ddd4f
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-01-12 00:34:12 +0530

    feat: ignore auth for list page

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,102 +1,40 @@
-import { useState, useEffect } from 'react';
-import { Alert } from "react-bootstrap";
-import BlogListItem from "./BlogListItem"
-import { listBlogs } from '../apis/blogs';
-import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+import BlogListItem from "./BlogListItem";
+import { listBlogs } from "../apis/blogs";
 
-const BlogList = ({isAuthenticated, setIsAuthenticated, searchResults}) => {
-  const [visible, setVisible] = useState(false);
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
-  const [deleteAlertMessage, setDeleteAlertMessage] = useState("");
-  const [alertVariant, setAlertVariant] = useState('');
-  const [loginError, setLoginError] = useState(null)
-  
+const BlogList = ({ searchResults }) => {
+  const [allBlogs, setAllBlogs] = useState([]);
+
   useEffect(() => {
-    // Check for token in cookies
-    const token = Cookies.get("token");
-
-    if (token) {
-      // Token is present, update isAuthenticated state
-      setIsAuthenticated(true);
-    } else {
-      // Token is not present, redirect to login page
-      setLoginError("Kindly login to see the posts")
-      setTimeout(() => {
-        setLoginError(null);
-      }, 3000);
+    if (searchResults.length > 0) {
+      setAllBlogs(searchResults);
     }
+  }, [searchResults]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const blogs = await listBlogs();
+        setAllBlogs(blogs.data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+
+    fetchBlogs();
   }, []);
 
-    const [allBlogs, setAllBlogs] = useState([]);
+  return (
+    <>
+      {allBlogs?.map((blog) => (
+        <BlogListItem
+          key={blog.id}
+          blog={blog}
+          setAllBlogs={setAllBlogs}
+        />
+      ))}
+    </>
+  );
+};
 
-    useEffect(() => {
-        if (searchResults.length > 0) {
-          setAllBlogs(searchResults);
-        }
-      }, [searchResults]);
-
-
-    useEffect(() => {
-        const fetchBlogs = async () => {
-            try {
-                const blogs = await listBlogs();
-                setAllBlogs(blogs.data);
-            } catch (error) {
-                console.error('Error fetching blogs:', error);
-            }
-        };
-
-        fetchBlogs();
-    }, []);
-
-    useEffect(() => {
-      let timer;
-      if (visible) {
-        timer = setTimeout(() => {
-          setVisible(false);
-        }, 3000);
-      }
-  
-      return () => clearTimeout(timer);
-    }, [visible]);
-  
-    useEffect(() => {
-      setVisible(showDeleteAlert);
-    }, [showDeleteAlert]);
-
-    return (
-      <>
-        {loginError?.length > 0 && (
-          <div className="alert alert-danger">{loginError}</div>
-        )}
-        {isAuthenticated && (
-          <>
-            <Alert
-              show={visible}
-              variant={alertVariant}
-              onClose={() => {
-                setVisible(false);
-              }}
-              dismissible
-            >
-              {deleteAlertMessage}
-            </Alert>
-            {allBlogs?.map((blog) => (
-              <BlogListItem
-                key={blog.id}
-                isAuthenticated={isAuthenticated}
-                blog={blog}
-                setAllBlogs={setAllBlogs}
-                setShowDeleteAlert={setShowDeleteAlert}
-                setDeleteAlertMessage={setDeleteAlertMessage}
-                setAlertVariant={setAlertVariant}
-                visible={visible}
-              />
-            ))}
-          </>
-        )}
-      </>
-    );
-}
-
-export default BlogList
+export default BlogList;
