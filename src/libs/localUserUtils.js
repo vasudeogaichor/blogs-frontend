bb62049ba278496f9defa294bf90d5a85d68a541
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-01-25 12:44:49 +0530

    fix: cleanup

--- src/libs/localUserUtils.js
+++ src/libs/localUserUtils.js
@@ -2,12 +2,8 @@ import Cookies from "js-cookie";
 
 export const setLocaUser = (data) => {
     Cookies.set("token", data.token);
-    // Cookies.set("user_id", data.user_id);
-    // Cookies.set("username", data.username);
-    // Cookies.set("email", data.email);
 }
 
 export const removeLocalUser = () => {
-    // ['token', 'user_id', 'username', 'email'].forEach(cookieName => { Cookies.remove(cookieName) })
     Cookies.remove('token')
 }
