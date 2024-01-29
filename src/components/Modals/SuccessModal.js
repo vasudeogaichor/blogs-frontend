ad50b0d1b13ff7af74c865fa1fd1e611679508ea
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-01-29 13:06:37 +0530

    feat: generalize SuccessModal

--- src/components/Modals/SuccessModal.js
+++ src/components/Modals/SuccessModal.js
@@ -1,13 +1,13 @@
 import { Modal, Button } from 'react-bootstrap';
 
-const SuccessModal = ({ showModal, closeModal }) => {
+const SuccessModal = ({ showModal, closeModal, successMessage }) => {
   return (
     <Modal show={showModal} onHide={closeModal}>
       <Modal.Header closeButton>
         <Modal.Title>Success!</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>Your blog has been successfully created / updated!</p>
+        <p>{successMessage || 'Operation successful'}</p>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={closeModal}>
